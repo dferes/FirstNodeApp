e52098047fb08aba356e4b2435c844ba41d21253
dferes <dferes23@gmail.com>
2021-02-07 21:44:45 -0800

    added some error catching for parameter type

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ExpressError = require('./expressError');
-const {getMean, getMode, getMedian} = require('./utilityFunctions')
+const {getMean, getMode, getMedian, validateNumberArray} = require('./utilityFunctions')
 
 const app = express();
 
@@ -9,40 +9,76 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome');
-})
+});
+
 
 app.get('/mean', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
 
+    const errorDetermination = validateNumberArray(req.query.nums.split(','));
+    if (errorDetermination instanceof Error) {
+        throw new ExpressError(errorDetermination.message);
+    }
+      
     return res.send({
         operation: 'mean',
         result: getMean(req.query.nums.split(','))
-      })
-})
+    })
+});
+
 
 app.get('/median', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
 
+    const errorDetermination = validateNumberArray(req.query.nums.split(','));
+    if (errorDetermination instanceof Error) {
+        throw new ExpressError(errorDetermination.message);
+    }
+
     return res.send({
         operation: 'median',
         result: getMedian(req.query.nums.split(','))
-      })
-})
+    })
+});
+
 
 app.get('/mode', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
+
+    const errorDetermination = validateNumberArray(req.query.nums.split(','));
+    if (errorDetermination instanceof Error) {
+        throw new ExpressError(errorDetermination.message);
+    }
+
     return res.send({
         operation: 'mode',
         result: getMode(req.query.nums.split(','))
-      })
-})
+    })
+});
+
+
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found",404);
+    return next(err);
+});
+
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+  
+    return res.json({
+      error: err,
+      message: err.message
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('App running on port 3000');
-})
+});
