b65e16912eda9e63b3ad986fa68480b47d020b44
dferes <dferes23@gmail.com>
2021-02-07 21:05:28 -0800

    json response now includes the operation type

--- app.js
+++ app.js
@@ -15,26 +15,34 @@ app.get('/mean', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
-    const mean = getMean(req.query.nums.split(','));
-    return res.status(200).json(mean);
+
+    return res.send({
+        operation: 'mean',
+        result: getMean(req.query.nums.split(','))
+      })
 })
 
 app.get('/median', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
-    const median = getMedian(req.query.nums.split(','));
-    return res.status(200).json(median);
+
+    return res.send({
+        operation: 'median',
+        result: getMedian(req.query.nums.split(','))
+      })
 })
 
 app.get('/mode', (req, res, next) => {
     if (!req.query.nums) {
         throw new ExpressError('A query key of numbers must be provided (separated by commas)', 400)
     }
-    const mode = getMode(req.query.nums.split(','));
-    return res.status(200).json(mode);
+    return res.send({
+        operation: 'mode',
+        result: getMode(req.query.nums.split(','))
+      })
 })
 
 app.listen(3000, () => {
     console.log('App running on port 3000');
-}) 
+})
