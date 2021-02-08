ea3e29f2730b19e123404ee563c13085ff7e5ae8
dferes <dferes23@gmail.com>
2021-02-07 21:46:29 -0800

    added validateNumberArray function

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -2,6 +2,7 @@ function getMean(arr) {
     return arr.reduce( (x,a) => Number(x)+Number(a))/arr.length;
 }
 
+
 function getMode(arr) {
     return Number(Object.values(
         arr.reduce( (count, e) => {
@@ -14,10 +15,16 @@ function getMode(arr) {
     ).reduce( (a, v) => v[0] < a[0] ? a: v, [0, null])[1]);
 }
 
+
 function getMedian(arr) {
     const sorted = arr.map(x => Number(x)).sort( (a,b) => a-b);
     return 0.5 * (sorted[Math.floor( (sorted.length-1)/2)] + sorted[Math.floor( (sorted.length)/2)]);
-    
 } 
 
-module.exports = {getMean, getMode, getMedian};
+function validateNumberArray(arr) {
+    numsArr = arr.map( num => Number(num));
+    return numsArr.includes(NaN) ? new Error('Only include numbers in the query string') : false;
+}
+
+
+module.exports = {getMean, getMode, getMedian, validateNumberArray};
